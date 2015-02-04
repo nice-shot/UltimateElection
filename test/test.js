faab20bab6671965ef13cd984f8e5e8eea8b3783
nice-shot <shefferu@gmail.com>
2015-02-05 00:46:44 +0200

    added test for multiple clients asking the same party

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 var server = require('../server.js');
 
-var expect = require('chai').expect;
+// var expect = require('chai').expect;
+var chai = require('chai');
+chai.should();
+
 var request = require('request');
 var WebSocket = require('ws');
 var cookieJar = request.jar();
@@ -16,8 +19,10 @@ describe('server response', function () {
 		server.close()
 	});
 
+	var url = "http://localhost:1234";
+	var urlWS = url.replace("http", "ws");
+
 	function getCookie(party, callback) {
-		var url = "http://localhost:1234";
 		request.post(url, {form: {party: party}}, function (err, res, body) {
 			var userCookie = cookieJar.getCookies(url)[0].value;
 			userCookie = decodeURIComponent(userCookie);
@@ -26,8 +31,6 @@ describe('server response', function () {
 	}
 
 	it('should return basic party data for once client', function (done) {
-		var url = "http://localhost:1234";
-		var urlWS = url.replace("http", "ws");
 		getCookie('LIE', function (userCookie) {
 			var socket = new WebSocket(urlWS);
 
@@ -37,8 +40,8 @@ describe('server response', function () {
 
 			socket.onmessage = function (message) {
 				var parsedData = JSON.parse(message.data);
-				expect(parsedData.score).to.equal(1);
-				expect(parsedData.users).to.equal(1);
+				parsedData.score.should.equal(1);
+				parsedData.users.should.equal(1);
 				done();
 			}
 		});
@@ -50,8 +53,22 @@ describe('server response', function () {
 		var currentUsers = 0;
 		var users = {};
 		for (var i=0; i < 5; i++) {
-			// users[i] = getCookie();
+			getCookie('LIE', function (cookie) {
+				users[i] = cookie;
+				currentUsers++;
+				var socket = new WebSocket(urlWS);
+				socket.onopen = function () {
+					socket.send(cookie);
+				}
+
+				socket.onmessage = function (message) {
+					var parsedData = JSON.parse(message.data);
+					parsedData.users.should.equal(currentUsers);
+				}
+			});
 		}
-		done();
+		setTimeout(function () {
+			done();
+		}, 300)
 	});
 });
