b24fc9f82f5840a729aa88f89332d9a6de9e489f
nice-shot <shefferu@gmail.com>
2015-02-09 22:03:18 +0200

    updated the create_db script - now works with index on votes

--- create_db.sql
+++ create_db.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE IF NOT EXISTS election;
-CREATE TABLE IF NOT EXISTS election.party (name VARCHAR(3) CHARSET utf8 PRIMARY KEY,
-										   votes BIGINT UNSIGNED NOT NULL INDEX)
+CREATE TABLE IF NOT EXISTS election.party (
+	name VARCHAR(3) CHARSET utf8,
+	votes BIGINT UNSIGNED NOT NULL,
+	PRIMARY KEY (name),
+	INDEX party_votes_ind (votes)
+)
