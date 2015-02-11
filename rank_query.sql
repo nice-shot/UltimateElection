fda0ae6ce48bdcddf2b127e3e9076b9862d65148
nice-shot <shefferu@gmail.com>
2015-02-11 23:29:51 +0200

    changed the rank query a bit so it will work with newer version of mysql

--- rank_query.sql
+++ rank_query.sql
@@ -3,9 +3,9 @@ SELECT ranked.name, ranked.votes, ranked.rank
           FROM election.party, (SELECT @rank:=0) AS init
          ORDER BY votes DESC) AS ranked,
        (SELECT rank
-          FROM (SELECT name, votes, @rank:=@rank + 1 AS rank
-                  FROM election.party, (SELECT @rank:=0) AS init
+          FROM (SELECT name, votes, @rank_a:=@rank_a + 1 AS rank
+                  FROM election.party, (SELECT @rank_a:=0) AS init
                  ORDER BY votes DESC) AS ranked
          WHERE name = ?) AS user_rank
  WHERE ranked.rank > user_rank.rank - 4
- 	   AND ranked.rank < user_rank.rank + 4;
+ 	   AND ranked.rank < user_rank.rank + 4
